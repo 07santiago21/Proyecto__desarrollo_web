9982e17d8fc78c2797c39032e880a59c93dbed32
juankkay010 <juanandrescuervoaguirre8@gmail.com>
2024-08-27 21:07:18 -0500

    read and insert in local storage

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,31 +1,34 @@
 function validate(){
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
+    let email = document.getElementById('email').value;
 
     let valid_username = /^[A-Za-z][A-Za-z0-9]{7,14}$/.test(username);
     let valid_password = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@*$#&])[A-Za-z0-9@$*#&]{12,20}$/.test(password);
     
     if(valid_username && valid_password){
-        read_validate_data(username, password)
+        insert_validate_data(username, password, email);
+        
     }
     else{
         alert("Ingrese bien los datos");
     }
-    
 }
 
+function insert_validate_data(username, password, email){
 
-function read_validate_data(username, password){
-    let json_data = localStorage.getItem(username);
-    let data = JSON.parse(json_data);
-    let storaged_password = data.password;
+    if(localStorage.getItem(username) == null)
+    {
+        let data = {
+            password: password,
+            email: email
+        }
     
-    if(storaged_password == password){
-        window.location.href="home.html"
+        localStorage.setItem(username, JSON.stringify(data));
+        window.location.href = "home.html";
     }
     else{
-        alert("Contraseña incorrecta")
+        alert("El usuario ya existe");
     }
     
 }
-
