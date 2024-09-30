ace5e6a46b17e0e4fa0668c4945cda63d380ba28
jeronimo <jerov79@gmail.com>
2024-09-30 17:26:54 -0500

    fixed sign-up

--- src/app/auth/pages/sign-up/sign-up.component.ts
+++ src/app/auth/pages/sign-up/sign-up.component.ts
@@ -1,12 +1,25 @@
+import { SignInComponent } from './../sign-in/sign-in.component';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
-  selector: 'app-sign-up',
+  selector: 'sign-up',
   standalone: true,
-  imports: [],
+  imports: [FormsModule], // Add FormsModule to imports
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'] // preguntar por tipo
 })
 export class SignUpComponent {
+  profile = {
+    username: '',
+    email: '',
+    password: '',
+    profile_picture: '',
+    bio: '',
+    is_owner: false
+  };
 
+  saveProfile() {
+    console.log('Profile saved', this.profile);
+  }
 }
