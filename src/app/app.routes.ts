e3f43288a25828720ae6a3ec45a4d4db2bd3eb65
jeronimo <jerov79@gmail.com>
2024-09-30 11:06:31 -0500

    erraser of add property component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { provideServerRendering } from '@angular/platform-server';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/pages/home/home.component';
 import { IndexComponent } from './index/index.component';
@@ -6,6 +7,9 @@ import { SignInComponent } from './auth/pages/sign-in/sign-in.component';
 import { SignUpComponent } from './auth/pages/sign-up/sign-up.component'; 
 import { ProfileSettingsComponent } from './features/profile-settings/pages/profile-settings/profile-settings.component';
 import { OwnerFilteringComponent } from './features/owner-filtering/pages/owner-filtering/owner-filtering.component';
+import { AddPropertyComponent } from './features/properties/add-property/add-property.component';
+
+
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -13,5 +17,6 @@ export const routes: Routes = [
   {path: 'sign-in', component: SignInComponent},
   {path: 'sign-up', component: SignUpComponent},
   {path: 'profile-settings', component: ProfileSettingsComponent},
-  {path: 'owner-filtering', component: OwnerFilteringComponent}
+  {path: 'owner-filtering', component: OwnerFilteringComponent},
+  {path: 'add-property', component: AddPropertyComponent}
 ];
--- src/app/features/properties/add-property/add-property.component.ts
+++ src/app/features/properties/add-property/add-property.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-add-property',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule], 
   templateUrl: './add-property.component.html',
-  styleUrl: './add-property.component.css'
+  styleUrls: ['./add-property.component.css']
 })
 export class AddPropertyComponent {
+  property = {
+    nombre: '',
+    precio: '',
+    lugar: ''
+    //poner mas campos ahora
+  };
+  imagePreview: string | ArrayBuffer | null = null;
 
+  // Método que se ejecuta cuando se envía el formulario
+  onSubmit() {
+    if (this.property.nombre && this.property.precio && this.property.lugar) {
+      console.log('Formulario enviado:', this.property);
+      // poner logica ak
+    }
+  }
+
+  // Metodo para manejar la selección de la imagen y mostrar una vista previa
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.imagePreview = reader.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
 }
