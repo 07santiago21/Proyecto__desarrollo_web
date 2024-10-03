4b5ab0702d60c3fd6d27a54ca67c7ee2e7e4977a
daniel <danielgiraldoroldan@hotmail.com>
2024-10-03 17:05:14 -0500

    ultimos cambio

--- src/app/features/properties/interfaces/property.interface.ts
+++ src/app/features/properties/interfaces/property.interface.ts
@@ -1,11 +1,13 @@
-export interface Property{
+export interface Property {
     listing_id: number;
     title: string;
     description: string;
     address: string;
+    latitude: string;
+    longitude: string;
     pricePerNight: number;
     rooms: number;
     bathrooms: number;
     maxCapacity: number;
     photos: string;
-}
+  }
