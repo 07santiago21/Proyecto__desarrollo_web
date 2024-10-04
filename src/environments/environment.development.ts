795184d434d646e77409c4ed330697d0780764c7
danielg2004 <giraldoroldandaniel@gmail.com>
2024-10-04 10:13:06 -0500

    Environment working

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    supabase: {
+        url: 'xx',
+        key: 'xx'
+    }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    supabase: {
+        url: 'https://mbjfebgwmqyeyudpenmr.supabase.co',
+        key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1iamZlYmd3bXF5ZXl1ZHBlbm1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc5OTEyNjQsImV4cCI6MjA0MzU2NzI2NH0.IUR4ndB8oJLOVHVZ8MrLQtcC2ni7fi-_yCDaV80ttjw'
+    }
+};
+
