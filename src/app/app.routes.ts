0918f10cca47126614e7aac1151cc5ff8d83d3e8
jeronimo <danielgiraldoroldan@hotmail.com>
2024-10-04 11:15:51 -0500

    some changes amde

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,7 @@ import { SignUpComponent } from './auth/pages/sign-up/sign-up.component';
 import { ProfileSettingsComponent } from './features/profile/pages/profile-settings/profile-settings.component';
 import { OwnerFilteringComponent } from './features/owner-filtering/pages/owner-filtering/owner-filtering.component';
 import { AddPropertyComponent } from './features/properties/add-property/add-property.component';
-import { EditPropertyComponent } from './features/properties/edit-property/edit-property.component';  
+
 import { PropertyEditComponent } from './features/properties/property-edit/property-edit.component';
 import { DetailsComponent } from './features/properties/details/details.component';
 
